8e42229e92bef0637da35ff33b1b6c65ac7b8497
verooroaltamirano <srtaoro@gmail.com>
2021-11-13 01:27:34 -0300

    nodemon

--- src/controllers/controllerProducts.js
+++ src/controllers/controllerProducts.js
@@ -1,4 +1,11 @@
-export const nombreFuncion = async (req, res) => {
-
+export const nombreFuncion =(req,res) => {
+    
     res.status(200).render('nombre_pagina_hbs')
 }
+export const verinicio = (req, res) => {
+    res.status(200).render('inicio')
+}
+export const verPrecio = (req, res) => {   
+    res.status(200).render('precio')
+}
+
--- src/routes/routesProducts.js
+++ src/routes/routesProducts.js
@@ -1,5 +1,10 @@
 import * as controllerProducts from '../controllers/controllerProducts.js'
-const routesProducts = app => {
-    app.get('/products', controllerProducts.nombreFuncion)
+
+const routesProducts = (app) => {
+    app.get('/products',controllerProducts.nombreFuncion)
+    app.get('/inicioPagina',controllerProducts.verinicio)
+    app.get('/precio',controllerProducts.verPrecio)
+
 }
 export default routesProducts
+
